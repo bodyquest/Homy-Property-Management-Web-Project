f35da2971b0d21e21a59acc07cb127bc0c5b7535
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-03-12 16:44:05 +0100

    Add ViewModels for "Create" Home from Admin

---
+++ Services/RPM.Services/Admin/Models/AdminCityDetailsServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RPM.Services.Admin.Models
+{
+    public class AdminCityDetailsServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
---
+++ Services/RPM.Services/Admin/Models/AdminHomeCreateServiceModel.cs
@@ -0,0 +1,36 @@
+﻿namespace RPM.Services.Admin.Models
+{
+    using AutoMapper;
+    using Microsoft.AspNetCore.Http;
+    using RPM.Data.Models;
+    using RPM.Data.Models.Enums;
+    using RPM.Services.Mapping;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class AdminHomeCreateServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public decimal Price { get; set; }
+
+        public HomeStatus Status { get; set; }
+
+        public HomeCategory Category { get; set; }
+
+        public string Owner { get; set; }
+
+        public CloudImage Image { get; set; }
+
+        public ICollection<string> Images { get; set; }
+    }
+}
