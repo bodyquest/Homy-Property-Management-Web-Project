7edd088ab916fd888335e99d7b83013373f69f29
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-03-20 21:46:41 +0100

    Add Views AllToRent / AllToManage

--- Web/RPM.Web/Controllers/ListingsController.cs
+++ Web/RPM.Web/Controllers/ListingsController.cs
@@ -86,5 +86,33 @@
 
             return this.View(viewModel);
         }
+
+        [ActionName("AllToRent")]
+        public async Task<IActionResult> AllToRentAsync()
+        {
+            var status = HomeStatus.ToRent.ToString();
+
+            var viewModel = new PropertyListByStatusViewModel
+            {
+                Properties = await this.listingService.GetAllByStatusAsync(status),
+                Status = status.ToString(),
+            };
+
+            return this.View(viewModel);
+        }
+
+        [ActionName("AllToManage")]
+        public async Task<IActionResult> AllToManageAsync()
+        {
+            var status = HomeStatus.ToManage.ToString();
+
+            var viewModel = new PropertyListByStatusViewModel
+            {
+                Properties = await this.listingService.GetAllByStatusAsync(status),
+                Status = status.ToString(),
+            };
+
+            return this.View(viewModel);
+        }
     }
 }
---
+++ Web/RPM.Web/Models/Listings/PropertyListByStatusViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RPM.Web.Models.Listings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using RPM.Services.Common.Models.Home;
+
+    public class PropertyListByStatusViewModel
+    {
+        public IEnumerable<PropertyListServiceModel> Properties { get; set; }
+
+        public string Status { get; set; }
+    }
+}
